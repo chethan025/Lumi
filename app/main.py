e8a27baf7305de8be5b6de2463de0709d9fcd815
Chethan <chethans0252@gmail.com>
2026-03-26 21:22:53 +0530

    Init

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from rich.console import Console
+
+
+console = Console()
+
+def main():
+    console.print("[bold cyan]Lumi v01[/bold cyan]")
+    console.print("[green]System initialized successfully.[/green]")
+
+if __name__ == "__main__":
+    main()
